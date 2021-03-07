20dee94e23b5fe0ca28ebcac8f4ccbc8a990cdf8
embed-coder <74451332+embed-coder@users.noreply.github.com>
2021-03-07 15:04:41 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,31 +1,30 @@
-class minecraft (
-  $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+ass minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
   $install_dir = '/opt/minecraft'
-) {
-  file { "$install_dir":
+){
+  file { $install_dir:
     ensure => directory,
   }
-  file { "$install_dir/minecraft_server.1.16.5.jar":
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft'],
   }
-  package { 'java':
+
+  package {'java':
     ensure => present,
   }
-  file { "$install_dir/eula.txt":
+  file {"${install_dir}/eula.txt":
     ensure => file,
-    content => 'eula=true',
+    content => 'eula=true'
   }
-  file { '/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => epp('minecraft/minecraft.service', {
-      install_dir => $install_dir
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
     }),
   }
   service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["$install_dir/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
