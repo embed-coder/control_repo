d7c031da86d5f474208321f8c3a374d944771f4e
embed-coder <74451332+embed-coder@users.noreply.github.com>
2021-03-07 15:45:44 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,8 +1,8 @@
-ass minecraft (
+class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
   $install_dir = '/opt/minecraft'
 ){
-  file { "$install_dir":
+  file { $install_dir:
     ensure => directory,
   }
   file { "${install_dir}/minecraft_server.jar":
